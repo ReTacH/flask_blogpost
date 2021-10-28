f82acda3ac6116a58d7ddcf457900f06413016bb
ReTacH <44493626+ReTacH@users.noreply.github.com>
2021-10-28 00:55:18 -0700

    updated spec of blogpost

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask, g, render_template, current_app
+from flask import Flask, g, render_template, request, current_app
 import sqlite3
+app = Flask(__name__)
 
 def getmessage():
-  if 'message_db' not in g:
-      g.message_db = sqlite3.connect('message_db.sqlite')
+	if 'message_db' not in g:
+    	    g.message_db = sqlite3.connect('message_db.sqlite')
 
-  with current_app.open_resource('init.sql') as f:
+	with current_app.open_resource('init.sql') as f:
 		g.message_db.executescript(f.read().decode('utf8'))
 
-  return g.message_db
+	return g.message_db
 
 def insert_message(request):
-  name = request.form['name']
+	name = request.form['name']
 	message = request.form['message']
 	db = get_message_db()
 	db.execute(
@@ -25,8 +26,28 @@ def insert_message(request):
 
 def random_messages(n):
 	db = get_message_db()
-	msg = db.execute(
+	rdm_message = db.execute(
 		f'SELECT handle, message FROM messages ORDER BY RANDOM() LIMIT {n}'
 		).fetchall()
 
-	return msg
+	return rdm_message
+
+
+@app.route('/')
+def main():
+    return render_template('base.html')
+
+@app.route('/submit/', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'GET':
+        return render_template('submit.html')
+    else:
+        try:
+            insert_message(request)
+            return render_template('submit.html', thanks = True)
+        except:
+            return render_template('submit.html')
+
+@app.route('/view/')
+def view():
+    return render_template('view.html', messages = rdm_message)
